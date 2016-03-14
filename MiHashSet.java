93e65aff3a1376ae27a38edcebc3f37223fcc4e5
Yggdrasil <ryuk1967@gmail.com>
2016-03-14 12:41:18 +0100

    Constructor

---
+++ MiHashSet.java
@@ -0,0 +1,141 @@
+
+public class MiHashSet
+{
+    private int[] coleccion;
+    
+    public MiHashSet()
+    {
+        coleccion = new int[0];
+    }
+
+    /**
+     * añade el elemento al conjunto si no estaba. 
+     * Devuelve verdadero en caso de que el elemento no estuviera presente en el conjunto y falso en caso contrario.
+     */
+    public boolean add(int valor)
+    {
+        boolean noContenido = true;      
+        int[] newColeccion = new int[coleccion.length+1];
+
+        for(int i = 0; i < coleccion.length && noContenido; i++){
+            if(coleccion[i] == valor){
+                noContenido = false;
+            }
+            else{
+                newColeccion[i] = coleccion[i];
+            }
+        }
+
+        if(noContenido){
+            newColeccion[newColeccion.length-1] = valor;
+            coleccion = newColeccion;
+        }
+
+        return noContenido;
+    }
+
+    /**
+     *  vacía el conjunto.
+     */
+    public void clear()
+    {
+        coleccion = new int[0];
+    }
+
+    /**
+     *  devuelve verdadero si el conjunto contiene el elemento, falso en caso contrario.
+     */
+    public boolean contains(int elemento)
+    { 
+        boolean contains = false;
+
+        for(int i = 0; i < coleccion.length && !contains; i++){
+            if(coleccion[i] == elemento){
+                contains = true;
+            }
+        }
+
+        return contains;
+    }
+
+    /**
+     *  devuelve verdadero si el conjunto no contiene elementos.
+     */
+    public boolean isEmpty()
+    {
+        return coleccion.length==0;
+
+    }
+
+    /**
+     * elimina del conjunto el elemento dado. Si no existiera devuelve falso; si existía en el conjunto devuelve verdadero.
+     */
+    public boolean remove(int elemento)
+    {
+        boolean eliminar = false;
+        if(coleccion.length != 0)
+        {
+            int[] newColeccion  = new int[coleccion.length - 1];
+            for(int index = 0; index < coleccion.length ; index++ )
+            {
+                if (coleccion[index] == elemento){
+                     eliminar = true;
+                }
+                else if(!eliminar){
+                    if(index != coleccion.length - 1 ){
+                        newColeccion[index]   = coleccion[index];
+                    }
+                } 
+                else{
+                    newColeccion[index - 1]   = coleccion[index];
+                }
+            }
+            if(eliminar){ 
+                coleccion   = newColeccion;
+            }
+        }
+        return eliminar;
+    }
+
+    /**
+     * devuelve el número de elementos del conjunto.
+     */
+    public int size(){
+        return coleccion.length;
+    }
+
+    /**
+     * devuelve una cadena conteniendo todos los elementos del conjunto separados por comas y entre corchetes.
+     */
+    public String toString(){
+        String toString = "[";
+        for(int i = 0; i < coleccion.length ; i++){
+            if(i == 0){
+                toString = toString  + coleccion[i];
+
+            }
+            else{ 
+                toString = toString + ", " + coleccion[i];	
+            }
+        }
+        toString = toString + "]";
+        return toString;
+    }
+
+    /**
+     * devuelve verdadero si el parámetro es igual al conjunto sobre el que se invoca y falso en otro caso.
+     */
+    public boolean equals(MiHashSet otroConjunto){
+        boolean igual = false;
+        if(otroConjunto.size() == size())
+        {
+            for(int i = 0;i < coleccion.length && igual ; i++){
+                if(!otroConjunto.contains(coleccion[i])){
+                    igual = true;
+                }
+            }
+        }
+
+        return igual;
+    }
+}
